ae386d07cc0e8611b76c312eb5aae971f790efe9
jacobrees <jacob.rees97@yahoo.com>
2020-07-24 14:53:35 +0100

    Complete exercises for nested_collections.rb in data types/colletions/advanced_nested_collections section didnt complete final 2 questions

--- data-types/collections/advanced_nested_collections/nesting_test.rb
+++ data-types/collections/advanced_nested_collections/nesting_test.rb
@@ -28,7 +28,7 @@ class NestedTest < MiniTest::Test
   def test_pancake_ingredients
     skip
     #=======================
-    # pancake_ingredients = <your code here>
+    pancake_ingredients = stores[:dennys][:dishes][0][:ingredients]
     #=======================
     assert_equal ["Flour", "Eggs", "Milk", "Syrup"], pancake_ingredients
   end
@@ -36,7 +36,7 @@ class NestedTest < MiniTest::Test
   def test_rissotto_price
     skip
     #=======================
-    # risotto_price = <your code here>
+    risotto_price = stores[:olive_garden][:dishes][0][:price]
     #=======================
     assert_equal 12, risotto_price
   end
@@ -44,7 +44,7 @@ class NestedTest < MiniTest::Test
   def test_big_mac_ingredients
     skip
     #=======================
-    # big_mac_ingredients = <your code here>
+    big_mac_ingredients = stores[:macdonalds][:dishes][0][:ingredients]
     #=======================
     assert_equal ['Bun','Hamburger','Ketchup','pickles'], big_mac_ingredients
   end
@@ -52,7 +52,7 @@ class NestedTest < MiniTest::Test
   def test_list_of_restaurants
     skip
     #=======================
-    # store_names = <your code here>
+    store_names = stores.keys
     #=======================
     assert_equal [:olive_garden, :dennys, :macdonalds], store_names
   end
@@ -60,7 +60,13 @@ class NestedTest < MiniTest::Test
   def test_list_of_dishes_names_for_olive_garden
     skip
     #=======================
-    # dishes_names = <your code here>
+    i = 0
+    dish_arr = []
+    while i < stores[:olive_garden][:dishes].size
+      dish_arr << stores[:olive_garden][:dishes][i][:name]
+      i += 1
+    end
+    dishes_names = dish_arr
     #=======================
     assert_equal ['Risotto', 'Steak'], dishes_names
   end
@@ -68,7 +74,13 @@ class NestedTest < MiniTest::Test
   def test_list_of_employees_across_all_restaurants
     skip
     #=======================
-    # employee_names = <your code here>
+    i = 0
+    emp_arr = []
+    while i < stores.size
+      emp_arr << stores[stores.keys[i]][:employees]
+      i += 1
+    end
+    employee_names = emp_arr.flatten
     #=======================
     assert_equal ["Jeff","Zach","Samantha","Bob","Sue","James","Alvin","Simon","Theodore"], employee_names
   end
@@ -76,6 +88,20 @@ class NestedTest < MiniTest::Test
   def test_list_of_all_ingredients_across_all_restaurants
     skip
     #=======================
+    i = 0
+    ing_arr = []
+    menu_arr = []
+    while i < stores.size
+      menu_arr << stores[stores.keys[i]][:dishes]
+      i += 1
+    end
+    menu_arr.flatten!
+    i = 0
+    while  i < menu_arr.size
+      ing_arr << menu_arr[i][:ingredients]
+      i += 1
+    end
+    ingredients = ing_arr.flatten
     # ingredients = <your code here>
     #=======================
     assert_equal ["Rice",
@@ -101,15 +127,21 @@ class NestedTest < MiniTest::Test
   def test_full_menu_price_for_olive_garden
     skip
     #=======================
-    # full_menu_price = <your code here>
+    i = 0
+    price_arr = []
+    while i < stores[:olive_garden][:dishes].size
+      price_arr << stores[:olive_garden][:dishes][i][:price]
+      i+=1
+    end
+    full_menu_price = price_arr.sum
     #=======================
     assert_equal 27, full_menu_price
   end
 
   def test_full_menu_for_olive_garden
-    skip
+    #skip
     #=======================
-    # olive_garden_menu = <your code here>
+    olive_garden_menu = stores[:olive_garden][:dishes]
     #=======================
     expected = ({"Risotto"=>{:name=>"Risotto", :ingredients=>["Rice", "Cheese", "Butter"], :price=>12},
                   "Steak"=>{:name=>"Steak", :ingredients=>["Beef", "Garlic"], :price=>15}})
