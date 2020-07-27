035f3b7a80ace77e2bf60e26009eef01be4afc6d
jacobrees <jacob.rees97@yahoo.com>
2020-07-27 18:04:30 +0100

    Complete select and select pattern challenges in enumerables exercises 1 section

--- enumerables/exercises_1/select_test.rb
+++ enumerables/exercises_1/select_test.rb
@@ -15,71 +15,71 @@ class SelectTest < Minitest::Test
   def test_pick_odd_numbers
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     odds = numbers.select do |number|
-      # Your code goes here
+      number.odd?
     end
     assert_equal [1, 3, 5, 7, 9], odds
   end
 
   def test_pick_words_with_three_letters
-    skip
+    #skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
+    selected = words.select {|word| word.length == 3}
     assert_equal ["bad", "cat", "dog", "red"], selected
   end
 
   def test_pick_words_with_more_than_three_letters
-    skip
+    #skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
+    selected = words.select {|word| word.length > 3}
     assert_equal ["pill", "finger", "blue", "table"], selected
   end
 
   def test_pick_words_ending_in_e
-    skip
+    #skip
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
-    # Your code goes here
+    selected = words.select {|word| word[-1] == "e"}
     assert_equal ["are", "strike", "piece", "warble", "pipe"], selected
   end
 
   def test_pick_words_ending_in_ing
-    skip
+    #skip
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
-    # Your code goes here
+    selected = words.select {|word| word[-3..-1] == "ing"}
     assert_equal ["bring", "singing"], selected
   end
 
   def test_pick_words_containing_e
-    skip
+    #skip
     words = ["four", "red", "five", "blue", "pizza", "purple"]
-    # Your code goes here
+    selected = words.select {|word| word.include?("e")}
     assert_equal ["red", "five", "blue", "purple"], selected
   end
 
   def test_pick_dinosaurs
-    skip
+    #skip
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
-    # Your code goes here
+    dinosaurs = animals.select {|word| word[-5..-1] == "aurus"}
     assert_equal ["tyrannosaurus", "achillesaurus", "qingxiusaurus"], dinosaurs
   end
 
   def test_pick_floats
-    skip
+    #skip
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
-    # Your code goes here
+    floats = numbers.select {|number| number.is_a? Float}
     assert_equal [1.4, 3.5, 4.9, 9.1, 8.0], floats
   end
 
   def test_pick_arrays
-    skip
+    #skip
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
-    # Your code goes here
+    arrays = elements.select {|array| array.is_a? Array}
     assert_equal [["dog"], [56, 3, 8]], arrays
   end
 
   def test_pick_hashes
-    skip
+    #skip
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
-    # Your code goes here
+    hashes = elements.select {|hash| hash.is_a? Hash}
     assert_equal [{:dog=>"fido"}, {:stuff=>"things"}], hashes
   end
 
